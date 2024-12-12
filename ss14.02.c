cf3989e62673ea92209ad06cb697b9b53604f11e
hoang1811-coder <vhoangssz1@gmail.com>
2024-12-12 14:57:05 +0700

    Add files via upload

---
+++ ss14.02.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string>
+
+int main() {
+    char string[] = "Hello, World!";
+    int length=strlen(string);
+    for (size_t i = 0; i < length(); ++i) {
+        std::cout << str[i] << " ";  
+    }
+
+    return 0;
+}
